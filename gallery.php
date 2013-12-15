fab76653bc60b6afbad143937843b7c0f8796050
tsquar3d <slugzzz@gmail.com>
2013-12-14 22:19:42 -0600

    first commit

---
+++ gallery.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <title>UberGallery</title>
+    <link rel="shortcut icon" href="resources/themes/uber-blue/images/favicon.png" />
+
+    <link rel="stylesheet" type="text/css" href="resources/themes/uber-blue/rebase-min.css" />
+    <link rel="stylesheet" type="text/css" href="resources/themes/uber-blue/style.css" />
+    <link rel="stylesheet" type="text/css" href="resources/colorbox/5/colorbox.css" />
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="resources/colorbox/jquery.colorbox.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("a[rel='colorbox']").colorbox({maxWidth: "90%", maxHeight: "90%", opacity: ".5"});
+    });
+</script>
+    
+</head>
+<body>
+
+<!-- Start UberGallery v2.4.5 - Copyright (c) 2013 Chris Kankiewicz (http://www.ChrisKankiewicz.com) -->
+<div id="galleryWrapper">
+    <h1>UberGallery</h1>
+    <div class="line"></div>
+
+    
+    <div id="galleryListWrapper">
+                    <ul id="galleryList" class="clearfix">
+                                    <li><a href="gallery-images/13891_594374027293769_805902481_n.jpg" title="13891 594374027293769 805902481 n" rel="colorbox"><img src="resources/cache/100x100-75-b34f2e61bfdc69e03d52a59deb4c041a.jpg" alt="13891 594374027293769 805902481 n"/></a></li>
+                                    <li><a href="gallery-images/996521_548511425213363_969294587_n.jpg" title="996521 548511425213363 969294587 n" rel="colorbox"><img src="resources/cache/100x100-75-863bffcbe2b9230ed6faecc47441378a.jpg" alt="996521 548511425213363 969294587 n"/></a></li>
+                                    <li><a href="gallery-images/998658_548511351880037_1703987647_n.jpg" title="998658 548511351880037 1703987647 n" rel="colorbox"><img src="resources/cache/100x100-75-89ca31e0f01a68b1636e2decc92e5336.jpg" alt="998658 548511351880037 1703987647 n"/></a></li>
+                                    <li><a href="gallery-images/1385752_600886366642535_678302258_n.jpg" title="1385752 600886366642535 678302258 n" rel="colorbox"><img src="resources/cache/100x100-75-e6d5ee5d48e40382d18b78040995545d.jpg" alt="1385752 600886366642535 678302258 n"/></a></li>
+                            </ul>
+            </div>
+
+    <div class="line"></div>
+    <div id="galleryFooter" class="clearfix">
+
+        
+        <div id="credit">Powered by, <a href="http://www.ubergallery.net">UberGallery</a></div>
+    </div>
+</div>
+<!-- End UberGallery - Distributed under the MIT license: http://www.opensource.org/licenses/mit-license.php -->
+
+</body>
+</html>
